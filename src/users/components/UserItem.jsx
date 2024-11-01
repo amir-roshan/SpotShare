b0c32de363928e39871b2f20ef1f3ebc705f8045
amir-roshan <amirvalielahi@gmail.com>
2024-10-31 19:45:07 -0700

    Add UserItem and Avatar components with styling; implement Card component for user display

--- src/users/components/UserItem.jsx
+++ src/users/components/UserItem.jsx
@@ -1,18 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import Card from '../../shared/components/UI/Card';
+import Avatar from '../../shared/components/UI/Avatar';
+
 import './UserItem.css';
+
 
 const UserItem = ({ id, image, name, placeCount }) => {
     return (
         <>
             <li className='user-item'>
-                <div className="user-item__content">
-                    <div className="user-item__image">
-                        <img src={image} alt={name} />
-                    </div>
-                    <div className="user-item__info">
-                        <h2>{name}</h2>
-                        <h3>{placeCount} {placeCount === 1 ? 'Place' : 'Places'}</h3>
-                    </div>
-                </div>
+                <Card className="user-item__content">
+                    <Link to={`/${id}/places`}>
+                        <div className="user-item__image">
+                            <Avatar image={image} alt={name} />
+                        </div>
+                        <div className="user-item__info">
+                            <h2>{name}</h2>
+                            <h3>{placeCount} {placeCount === 1 ? 'Place' : 'Places'}</h3>
+                        </div>
+                    </Link>
+                </Card>
             </li>
         </>
     );
