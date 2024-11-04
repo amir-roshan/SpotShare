fb8809023c23d07abfa4dfef7558897e37a91adb
amir-roshan <amirvalielahi@gmail.com>
2024-11-04 11:52:02 -0800

    Implement side drawer navigation with backdrop; add transition effects and update HTML structure

--- src/shared/Navigation/MainNavigation.jsx
+++ src/shared/Navigation/MainNavigation.jsx
@@ -1,15 +1,33 @@
+import './MainNavIgation.css';
+
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
-
-import './MainNavIgation.css';
+import SideDrawer from './SideDrawer';
+import { useState } from 'react';
+import Backdrop from '../components/UI/Backdrop';
 
 const MainNavigation = () => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    };
     return (
         <>
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                <nav className='main-navigation__drawer-nav'>
+                    <NavLinks />
+                </nav>
+            </SideDrawer >
             <MainHeader>
-                <button className='main-navigation__menu-btn'>
+                <button onClick={openDrawerHandler} className='main-navigation__menu-btn'>
                     <span />
                     <span />
                     <span />
@@ -19,7 +37,7 @@ const MainNavigation = () => {
                         SpotShare
                     </Link>
                 </h1>
-                <nav>
+                <nav className='main-navigation__header-nav'>
                     <NavLinks />
                 </nav>
             </MainHeader>
@@ -27,4 +45,4 @@ const MainNavigation = () => {
     );
 };
 
-export default MainNavigation;
+export default MainNavigation;;
