b22e343e747f7b8c5772c761c199fb9bb3ee6724
amir-roshan <amirvalielahi@gmail.com>
2024-11-09 23:45:59 -0800

    Add input handling for title and description in NewPlace form; implement validation and update Input component

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,17 +1,37 @@
+import { useCallback } from "react";
+
 import "./NewPlace.css";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/utils/validators";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <>
       <form className="place-form">
         <Input
+          id="title"
           type="text"
           label="Title"
           element="input"
           validators={[VALIDATOR_REQUIRE()]}
           error={"Please enter a valid title."}
+          onInput={titleInputHandler}
+        ></Input>
+        <Input
+          id="description"
+          type="text"
+          label="Title"
+          element="input"
+          validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+          error={"Please enter a valid description (at least 5 characters)."}
+          onInput={descriptionInputHandler}
         ></Input>
       </form>
     </>
--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import "./Input.css";
 import { validate } from "../../utils/validators";
 
@@ -29,12 +29,19 @@ const Input = ({
   rows,
   validators,
   error,
+  onInput,
 }) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
     isValid: false,
     isTouch: false,
   });
+
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
 
   const changeHandler = (event) => {
     dispatch({
